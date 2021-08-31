7b06fe51727295f33cea875ea1de0ecb6d862eb1
Devron  Tombacco <tombacco.devron@gmail.com>
2021-08-31 10:07:09 +0100

    add tableView to MyDecksVC

--- Memorius/Memorius/MyDecksVC.swift
+++ Memorius/Memorius/MyDecksVC.swift
@@ -9,21 +9,43 @@ import UIKit
 
 class MyDecksVC: UIViewController {
 
+    let decksTableView = UITableView()
+    var testDataArray = ["Deck 1", "Deck 2", "Deck 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
-        navigationItem.title = "Home"
-        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 40))
-        view.addSubview(button)
-        button.setTitle("push second controller", for: .normal)
-        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        navigationItem.title = "My Decks"
+        view.addSubview(decksTableView)
+        decksTableView.dataSource = self
+        decksTableView.register(UITableViewCell.self, forCellReuseIdentifier: "DeckTableViewCell")
+        configureTableView()
     }
     
-    @objc func buttonClicked(){
-        let secondController = SecondViewController()
-        navigationController?.pushViewController(secondController, animated: true)
-    }
+    func configureTableView(){
+        decksTableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+                decksTableView.topAnchor.constraint(equalTo: view.topAnchor),
+                decksTableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+                decksTableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+                decksTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+        }
+}
 
+extension MyDecksVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return testDataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DeckTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = testDataArray[indexPath.row]
+        
+        return cell
+    }
 }
 
 class SecondViewController: UIViewController {
@@ -35,3 +57,20 @@ class SecondViewController: UIViewController {
     }
 
 }
+
+
+
+//        decksTableView.delegate = self
+
+
+//    func configureButton(){
+//        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 200, height: 40))
+//        view.addSubview(button)
+//        button.setTitle("push second controller", for: .normal)
+//        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+//    }
+//
+//    @objc func buttonClicked(){
+//        let secondController = SecondViewController()
+//        navigationController?.pushViewController(secondController, animated: true)
+//    }
