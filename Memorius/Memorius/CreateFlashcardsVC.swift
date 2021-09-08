edd026522524ba84bae762a7bd82d7af3c329641
Devron  Tombacco <tombacco.devron@gmail.com>
2021-09-08 17:52:30 +0100

    Add CreateFlashcardsVS

---
+++ Memorius/Memorius/CreateFlashcardsVC.swift
@@ -0,0 +1,29 @@
+//
+//  CreateFlashcardsVC.swift
+//  Memorius
+//
+//  Created by Devron Tombacco on 08/09/2021.
+//
+
+import UIKit
+
+class CreateFlashcardsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
