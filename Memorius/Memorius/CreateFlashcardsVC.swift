b99ad7fa8ab18f80adc32e62fe5bccc9fa498987
Devron  Tombacco <tombacco.devron@gmail.com>
2021-09-08 18:16:20 +0100

    Add UIElements to CreateFlashcardsVC

--- Memorius/Memorius/CreateFlashcardsVC.swift
+++ Memorius/Memorius/CreateFlashcardsVC.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class CreateFlashcardsVC: UIViewController {
 
+    // MARK:-- UIElements
+    let flascardFrontText = UILabel()
+    let flascardBackText = UILabel()
+    let flashcardFrontTextfield = UITextField()
+    let flashcardBackTextfield = UITextField()
+    let deckPicker = UIPickerView()
+    let createButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .white
         navigationItem.title = "Create Flashcards"
-        
     }
 
+
+    
 
 }
