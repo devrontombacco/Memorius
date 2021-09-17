ed1d9ad6d1b340f3c1bb9ef113e8fd0eff39597a
Devron  Tombacco <tombacco.devron@gmail.com>
2021-09-17 17:49:17 +0100

    Add Flashcard struct to Model folder

---
+++ Memorius/Memorius/Model/Flashcard.swift
@@ -0,0 +1,14 @@
+//
+//  Flashcard.swift
+//  Memorius
+//
+//  Created by Devron Tombacco on 17/09/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Flashcard {
+    let question: String
+    let answer: String
+}
