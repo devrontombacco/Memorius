36a31d4b67e86e21ba52033f73108cad1a802949
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-11 19:18:05 +0100

    Add labels to stackView

--- Memorius/Memorius/Controller/RunthroughVC.swift
+++ Memorius/Memorius/Controller/RunthroughVC.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class RunthroughVC: UIViewController {
 
+    // MARK: -- UILABELS
+    let deckRunthroughNoLabel = UILabel()
+    let deckFlashcardNoLabel = UILabel()
+    
     // MARK: -- UISTACKVIEWS
     let deckDetailsStackView = UIStackView()
     
@@ -18,9 +22,50 @@ class RunthroughVC: UIViewController {
         title = "Runthrough: (Deck Name Here)"
         
         view.addSubview(deckDetailsStackView)
+        view.addSubview(deckRunthroughNoLabel)
+        view.addSubview(deckFlashcardNoLabel)
+        
+        configureDeckLabels()
+        configureDeckDetailsStackView()
+        configuredeckDetailsStackViewLabelImages()
+    }
+    
+    func configureDeckLabels() {
+        
+        deckRunthroughNoLabel.translatesAutoresizingMaskIntoConstraints = false
+        deckRunthroughNoLabel.text = " Runthroughs"
+        deckRunthroughNoLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        deckRunthroughNoLabel.textColor = .systemBlue
+        deckRunthroughNoLabel.backgroundColor = .white
+        deckRunthroughNoLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        deckRunthroughNoLabel.textAlignment = .center
+        
+        deckFlashcardNoLabel.translatesAutoresizingMaskIntoConstraints = false
+        deckFlashcardNoLabel.text = " Runthroughs"
+        deckFlashcardNoLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        deckFlashcardNoLabel.textColor = .systemBlue
+        deckFlashcardNoLabel.backgroundColor = .white
+        deckFlashcardNoLabel.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        deckFlashcardNoLabel.textAlignment = .center
 
     }
+    
+    func configureDeckDetailsStackView(){
+        
+        deckDetailsStackView.translatesAutoresizingMaskIntoConstraints = false
+        deckDetailsStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+        deckDetailsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 
+        deckDetailsStackView.addArrangedSubview(deckRunthroughNoLabel)
+        deckDetailsStackView.addArrangedSubview(deckFlashcardNoLabel)
+
+        deckDetailsStackView.axis = .horizontal
+        deckDetailsStackView.backgroundColor = .white
+        deckDetailsStackView.distribution = .fillEqually
+        deckDetailsStackView.spacing = 30
+        deckDetailsStackView.alignment = .center
+        
+    }
 
 
 
