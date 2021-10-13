acae10812072605e8966167f35cd565797832463
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-13 19:34:09 +0100

    Add ScoreVC

---
+++ Memorius/Memorius/Controller/ScoreVC.swift
@@ -0,0 +1,20 @@
+//
+//  ScoreVC.swift
+//  Memorius
+//
+//  Created by Devron Tombacco on 13/10/2021.
+//
+
+import UIKit
+
+class ScoreVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
