c0a247a5da25318f43d79204d1c51b903ca46320
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-14 17:39:52 +0100

    Add runthroughStatsStackView to VC

--- Memorius/Memorius/Controller/ScoreVC.swift
+++ Memorius/Memorius/Controller/ScoreVC.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class ScoreVC: UIViewController {
-
+    
+    // MARK:-- UILABELS
     let scoreLabel = UILabel()
+
+    // MARK:-- UISTACKVIEWS()
+    let runthroughStatsStackView = UIStackView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +22,9 @@ class ScoreVC: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(scoreLabel)
+        view.addSubview(runthroughStatsStackView)
         configureScoreLabel()
+        configureRunthroughStatsStackView()
     }
     
     func configureScoreLabel(){
@@ -38,6 +44,21 @@ class ScoreVC: UIViewController {
         scoreLabel.adjustsFontSizeToFitWidth = true
     }
     
-
+    func configureRunthroughStatsStackView() {
+        
+        runthroughStatsStackView.translatesAutoresizingMaskIntoConstraints = false
+        runthroughStatsStackView.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor, constant: 40).isActive = true
+        runthroughStatsStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        runthroughStatsStackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        runthroughStatsStackView.addArrangedSubview(deckRunthroughNoLabel)
+        runthroughStatsStackView.addArrangedSubview(deckFlashcardNoLabel)
+       
+        runthroughStatsStackView.axis = .vertical
+        runthroughStatsStackView.backgroundColor = .white
+        runthroughStatsStackView.distribution = .fillEqually
+        runthroughStatsStackView.spacing = 30
+        runthroughStatsStackView.alignment = .center
+        
+    }
 
 }
