023e25659e210f70e976d0d576ed7d0946c8264f
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-14 18:11:37 +0100

    Add Repeat Button and Review Button

--- Memorius/Memorius/Controller/ScoreVC.swift
+++ Memorius/Memorius/Controller/ScoreVC.swift
@@ -25,6 +25,11 @@ class ScoreVC: UIViewController {
     // MARK:-- UISTACKVIEWS()
     let runthroughStatsStackView = UIStackView()
     
+    
+    // MARK: -- UIBUTTONS
+    let repeatButton = UIButton()
+    let reviewAnswersButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,10 +38,15 @@ class ScoreVC: UIViewController {
         
         view.addSubview(scoreLabel)
         view.addSubview(runthroughStatsStackView)
+        view.addSubview(repeatButton)
+        view.addSubview(reviewAnswersButton)
+        
         configureScoreLabel()
         configureRunthroughStatsStackView()
         configureStackViewLabels()
         configureRunthroughStackViewLabelImages()
+        configureReviewAnswersButton()
+        configureRepeatButton()
     }
     
     func configureScoreLabel(){
@@ -134,5 +144,48 @@ class ScoreVC: UIViewController {
         runthroughsLabel.attributedText = runthroughsImageFullstring
         
     }
+    
+    func configureReviewAnswersButton(){
+        
+        reviewAnswersButton.translatesAutoresizingMaskIntoConstraints = false
+        reviewAnswersButton.topAnchor.constraint(equalTo: runthroughStatsStackView.bottomAnchor, constant: 60).isActive = true
+        reviewAnswersButton.trailingAnchor.constraint(equalTo: view.trailingAnchor,  constant: -20).isActive = true
+        
+        reviewAnswersButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        reviewAnswersButton.widthAnchor.constraint(equalToConstant: 148).isActive = true
+        reviewAnswersButton.layer.cornerRadius = 10
+        reviewAnswersButton.setTitle("REVIEW", for: .normal)
+        reviewAnswersButton.backgroundColor = .systemBlue
+        reviewAnswersButton.setTitleColor(.white, for: .normal)
+        reviewAnswersButton.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        reviewAnswersButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        reviewAnswersButton.addTarget(self, action: #selector(didTapReviewAnswersButton), for: .touchUpInside)
 
+    }
+    
+    @objc private func didTapReviewAnswersButton() {
+        print("Review Answers button tapped")
+    }
+    
+    func configureRepeatButton() {
+        
+        repeatButton.translatesAutoresizingMaskIntoConstraints = false
+        repeatButton.topAnchor.constraint(equalTo: runthroughStatsStackView.bottomAnchor, constant: 60).isActive = true
+        repeatButton.leadingAnchor.constraint(equalTo: view.leadingAnchor,  constant: 20).isActive = true
+        repeatButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        repeatButton.widthAnchor.constraint(equalToConstant: 148).isActive = true
+        repeatButton.layer.cornerRadius = 10
+        repeatButton.setTitle("REPEAT", for: .normal)
+        repeatButton.backgroundColor = .systemBlue
+        repeatButton.setTitleColor(.white, for: .normal)
+        repeatButton.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        repeatButton.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        repeatButton.addTarget(self, action: #selector(didTapRepeatButton), for: .touchUpInside)
+        
+    }
+    
+    @objc private func didTapRepeatButton() {
+        print("Repeat button tapped")
+    }
+    
 }
