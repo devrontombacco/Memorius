3e552df7981a7f92ea7ffecb8bf9dad1c0d2acb0
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-21 12:01:58 +0100

    Add createNewDeckLabel

--- Memorius/Memorius/Controller/CreateDecksVC.swift
+++ Memorius/Memorius/Controller/CreateDecksVC.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class CreateDecksVC: UIViewController {
 
+    // MARK:-- UILabels
+    let createNewDeckLabel = MemSectionHeading()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.addSubview(createNewDeckLabel)
+        
+        configureCreateNewDeckLabel()
+    }
+    
+    func configureCreateNewDeckLabel(){
+        
+        createNewDeckLabel.text = "New Deck name"
+        createNewDeckLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 43).isActive = true
+        createNewDeckLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        createNewDeckLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 20).isActive = true
+        createNewDeckLabel.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
     }
     
 
