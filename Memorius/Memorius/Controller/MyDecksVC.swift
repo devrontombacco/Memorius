d7ad0b276c9c35ea7759af9872b0a4c77d055598
Devron  Tombacco <tombacco.devron@gmail.com>
2021-10-23 12:23:39 +0100

    Make Searchbar display Deck Names from Realm

--- Memorius/Memorius/Controller/MyDecksVC.swift
+++ Memorius/Memorius/Controller/MyDecksVC.swift
@@ -11,43 +11,49 @@ import RealmSwift
 class MyDecksVC: UIViewController {
 
     // MARK:-- Variables
-//    var filteredData : [String] = []
-    
+    var filteredData : [String] = []
+    var deckNamesArray : [String] = []
     var newFlashcard = Flashcard()
     var newDeck = Deck(name: "")
     
+    // MARK: Realm Variables
     let realm = try! Realm()
-    var realmDataArray = try! Realm().objects(Flashcard.self).sorted(byKeyPath: "question", ascending: true)
+    var flashcardDataArray = try! Realm().objects(Flashcard.self).sorted(byKeyPath: "question", ascending: true)
     var deckDataArray = try! Realm().objects(Deck.self).sorted(byKeyPath: "name", ascending: true)
     
     // MARK:-- UIElements
     let decksTableView = UITableView()
     let deckSearchBar = UISearchBar()
-    
-    // DECKS
-    let biologyDeck = Deck(name: "Biology")
-    
+
     // MARK:-- ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // VC setup
+        navigationItem.title = "My Decks"
         view.backgroundColor = .white
         view.addSubview(deckSearchBar)
-        navigationItem.title = "My Decks"
         view.addSubview(decksTableView)
+        configureNavigationController()
+        
+        // tableView setup
         decksTableView.delegate = self
         decksTableView.dataSource = self
         decksTableView.register(UITableViewCell.self, forCellReuseIdentifier: "DeckTableViewCell")
+        
+        // UI setup
         configureTableView()
         configureSearchbar()
-        deckSearchBar.delegate = self
-        configureNavigationController()
-//
-//        filteredData = testDeckNamesl
-        
-        
+  
         // Realm configuration
         let path = realm.configuration.fileURL?.path
         print("Path: \(String(describing: path))")
+        
+        
+        // Searchbar filter
+        deckSearchBar.delegate = self
+        createDeckNamesArray()
+        filteredData = deckNamesArray
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -64,15 +70,17 @@ class MyDecksVC: UIViewController {
     
     // MARK: UI Configuration Functions
     func configureSearchbar(){
+        
         deckSearchBar.translatesAutoresizingMaskIntoConstraints = false
         deckSearchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         deckSearchBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         deckSearchBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        
         deckSearchBar.showsCancelButton = true
+        
     }
 
     func configureTableView(){
+        
         decksTableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
                 decksTableView.topAnchor.constraint(equalTo: deckSearchBar.bottomAnchor),
@@ -80,17 +88,33 @@ class MyDecksVC: UIViewController {
                 decksTableView.rightAnchor.constraint(equalTo: view.rightAnchor),
                 decksTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             ])
+        
     }
     
     func configureNavigationController(){
+        
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewDeck))
+        
     }
 
     @objc func addNewDeck(){
+        
         print("Add new deck button clicked")
         let nextVC = CreateDecksVC()
         nextVC.delegate = self
         navigationController?.pushViewController(nextVC, animated: true)
+        
+    }
+    
+    func createDeckNamesArray(){
+        
+        try! realm.write {
+            for deck in deckDataArray {
+                deckNamesArray.append(deck.name)
+                print("deckNamesArry contains: \(deck.name)")
+            }
+        }
+        
     }
 
 }
@@ -100,17 +124,16 @@ extension MyDecksVC: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return deckDataArray.count
+        return filteredData.count
     
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "DeckTableViewCell", for: indexPath)
-        cell.textLabel?.text = deckDataArray[indexPath.row].name
+        cell.textLabel?.text = filteredData[indexPath.row]
         return cell
     }
-    
     
 }
 
@@ -131,33 +154,38 @@ extension MyDecksVC: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
         // reset searchBar
-//        filteredData = []
+        filteredData = []
         
         // Make items appear in tableView if searchbar is empty
-//         if searchText == "" {
-//            print("searchbar empty")
-//            let filteredDeckNames = testDeckNames.map {$0}
-//            filteredData = filteredDeckNames
-//
-//        }
+         if searchText == "" {
+            
+            print("searchbar empty")
+            let filteredDeckNames = deckNamesArray.map {$0}
+            filteredData = filteredDeckNames
+            
+        }
         
-        // Match searchbar input to testDeckArray
-//        for deck in testDeckNames {
-//            if deck.uppercased().contains(searchText.uppercased()){
-//                filteredData.append(deck)
-//                print(filteredData)
-//            }
-//
-//        }
+        // Match searchbar input to deckDataArray
+        for deck in deckNamesArray {
+            
+            if deck.uppercased().contains(searchText.uppercased()){
+                filteredData.append(deck)
+                print(filteredData)
+            }
+            
+        }
         
         // reload tableView
         self.decksTableView.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        
         deckSearchBar.resignFirstResponder()
         deckSearchBar.text? = ""
+        
     }
+    
 }
 
 extension MyDecksVC: CreateDecksVCDelegate {
@@ -171,13 +199,4 @@ extension MyDecksVC: CreateDecksVCDelegate {
 
     }
 
-//extension MyDecksVC: CreateFlashcardsVCDelegate {
-//
-//    func createCard(newlyCreatedFlashcard: Flashcard) {
-//
-//        newFlashcard = newlyCreatedFlashcard
-//        addToRealm(toAdd: newFlashcard)
-//    }
-//
-//}
 }
